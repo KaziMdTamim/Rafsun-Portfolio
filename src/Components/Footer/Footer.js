a94d1212a2c018cffc99c2c0a089e710870e717c
Kazi Md. Tamim <kazimdtamim01@gmail.com>
2021-12-01 02:13:45 +0600

    Alhamdulillah-done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='footer-part'>
             <h4>Copyright © 2021 All Rights Reserved</h4>
             <br />
-            <h6>Visit My Profile: <a href="https://www.facebook.com/rafsan.rohan29">Facebook@Rafsan</a></h6>
+            <h6>Visit My Profile: <a href="https://www.facebook.com/rafsan.rohan29" target="blank">Facebook@Rafsan</a></h6>
         </div>
     );
 };
